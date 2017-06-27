398a3f792184127d5cbc0eb39d92e00c48b91ff4
Varabe <zertarx@gmail.com>
2017-06-27 06:21:36 +0300

    [add] .gitignore

--- scripts/s.py
+++
@@ -1,9 +0,0 @@
-from lib.commands import database
-from lxml import etree as XML
-
-guilds = database.find("guilds").iterchildren()
-for guild in guilds:
-	element = XML.SubElement(guild, "achi")
-	element.text = ""
-
-database.rewrite()
