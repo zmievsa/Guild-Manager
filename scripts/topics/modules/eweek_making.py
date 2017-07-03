1c3a91bac571e012c71eb2cb82ee0ce3c4a56473
Varabe <zertarx@gmail.com>
2017-07-03 13:38:36 +0300

    [add] eweek_making topic

---
+++ scripts/topics/modules/eweek_making.py
@@ -0,0 +1,27 @@
+from lib.database_objects import createEweek
+from lib.config import test_id
+from topics.lib import Fields
+
+
+id = 35748154
+group_id = test_id
+comment_amount = 83
+
+
+def getAction(text):
+	return main
+
+
+def getResponse(request):
+	return "Еженедельник успешно создан."
+
+
+def finish(request):
+	pass
+
+
+def main(request):
+	mandatory_keys = {"карта":"map", "челленджи":"challenges"}
+	optional_keys = {"сложность":"diff", "цель":"goal", "дополнительно":"settings"}
+	fields = Fields(request.text, mandatory_keys, optional_keys)
+	createEweek(**fields)
