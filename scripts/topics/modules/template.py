362108a220ba51da01cf1153f6499f030a6e9cb5
Varabe <zertarx@gmail.com>
2017-07-03 12:58:44 +0300

    [add] template to ease the creation of new topics

---
+++ scripts/topics/modules/template.py
@@ -0,0 +1,30 @@
+from topics.lib import Fields, getPhoto
+from lib.config import group_id, test_id
+
+
+id = 000
+group_id = test_id
+comment_amount = 83
+
+
+def getAction(text):
+	return main
+
+
+def getResponse(request):
+	return "Заявка успешна."
+
+
+def finish(request):
+	pass
+
+
+def main(request):
+	mandatory_keys = {"name":"название",}
+	optional_keys = {}
+	fields = Fields(request.text, mandatory_keys, optional_keys)
+	editFields(fields)
+
+
+def editFields(fields):
+	pass
