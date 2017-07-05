15ccdbe4da9fb722c8ac9d6add314f0e0836bc65
Varabe <zertarx@gmail.com>
2017-07-05 19:19:04 +0300

    [add] emergency id for error notification

--- scripts/lib/errors.py
+++ scripts/lib/errors.py
@@ -1,8 +1,8 @@
 """ Оповещение администратора о возникших ошибках """
 
 from traceback import format_exception, format_exc
+from lib.config import emergency_id
 from lib.commands import vk, api
-from lib.config import my_id
 
 
 class ErrorManager:
@@ -26,7 +26,7 @@ def sendErrorMessage(name, exception=None):
 	""" Использует либо полученную ошибку, либо ту, что возникла последней """
 	exception = format_error(exception)
 	message = "{}:\n{}".format(name, exception)
-	vk(api.messages.send, user_id=my_id, message=message)
+	vk(api.messages.send, user_id=emergency_id, message=message)
 
 
 def format_error(error):
