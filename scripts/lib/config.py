ef28e5f5b6853d92c82dccc30a3c66650a03ab53
Varabe <zertarx@gmail.com>
2017-09-21 14:10:36 +0300

    PEP8 config.py

--- scripts/lib/config.py
+++ scripts/lib/config.py
@@ -8,16 +8,18 @@ std_avatar_id = 29						# id стандартного аватара
 sleep_time = 0.65						# Время ожидания sleep()
 data_folder = "../Data/"				# Путь от GM4/scripts к GM4/Data
 topics_folder = "topics/modules/"		# Путь к модулям для топиков
-offline_debug = True					# При оффлайн-работе
+offline_debug = False					# При оффлайн-работе
 
 
 """ TOPICS """
+
 
 class Image:
 	""" Упрощенное взаимодействие с атрибутами изображений """
 	def __init__(self, link):
 		self.link = link
 		self.id = int(link.split("_")[1])
+
 
 failure_image = Image("photo-77675108_456239404")	# Ссылка на фото, когда ГМ нашел ошибку
 succeed_image = Image("photo-77675108_456239403")	# Ссылка на фото, когда ГМ успешно все поменял
